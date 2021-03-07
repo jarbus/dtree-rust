6b8e63e5a2998f3fa854bb386522466cd28b6911
jarbus <jarbus@tutanota.com>
2021-03-06 23:54:19 -0500

    test

--- src/main.rs
+++ src/main.rs
@@ -13,58 +13,6 @@ use piston::{ButtonEvent, RenderEvent};
 
 use opengl_graphics::{GlGraphics, OpenGL };
 
-type Color = [f32; 4];
-
-const RED: Color = [1.0, 0.0, 0.0, 1.0];
-const GREEN: Color = [0.0, 1.0, 0.0, 1.0];
-const BLUE: Color = [0.0, 0.0, 1.0, 1.0];
-const WHITE: Color = [1.0, 1.0, 1.0, 1.0];
-const BLACK: Color = [0.0, 0.0, 0.0, 1.0];
-
-
-#[derive(Clone)]
-enum Shape {
-    Rect,
-    Circle
-}
-
-#[derive(Clone)]
-pub struct Node {
-   shape: Shape,
-   pos: [f64; 2],
-   rot: f32,
-   size: f64,
-}
-
-impl Node{
-    fn draw(&self, c: graphics::context::Context, gl: &mut GlGraphics) {
-        if let Some(v) = c.viewport{
-            let x0 = (self.pos[0] * v.window_size[0]) - (self.size/2.0);
-            let y0 = (self.pos[1] * v.window_size[1]) - (self.size/2.0);
-
-            match self.shape {
-                Shape::Rect => graphics::Rectangle::new(WHITE).draw([x0, y0,self.size, self.size], &c.draw_state, c.transform, gl),
-                Shape::Circle => graphics::CircleArc::new(WHITE,100.0,0.0,2.0).draw([x0,y0,self.size,self.size], &c.draw_state, c.transform, gl),
-            }
-        }
-    }
-    fn new(start_shape: Shape) -> Node {
-        Node {
-            shape: start_shape,
-            pos: [0.5, 0.5],
-            rot: 0.0,
-            size: 100.0,
-        }
-    }
-}
-
-type Graph = Vec<Node>;
-
-fn make_graph() -> Graph {
-    let graph = vec![Node::new(Shape::Rect); 1];
-    graph
-}
-
 fn main() {
     let opengl = OpenGL::V3_2;
     let settings = WindowSettings::new("Roguelike", [512; 2]).exit_on_esc(true);
